6c94f46c570b1f645054af80acfd9ffaa20fce52
jaya999 <jayaror1991@gmail.com>
2018-06-23 17:09:41 +0530

    Complete app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :channels
+  resources :discussions do
+  	resources :replies
+  end
+  root 'discussions#index'
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
